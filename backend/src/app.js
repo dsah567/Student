4b37497c9e0aed02348f6883ac996be1bbbc221c
Dipu Sah <sahd7929@gmail.com>
2024-12-20 00:21:06 +0530

    layout managed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import studentRoutes from "./routes/student.route.js";
 
 const app = express();
+
+const allowedOrigins = "http://localhost:5173,https://.vercel.app".split(',');
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, // Allow credentials (cookies, authorization headers)
+}));
+
 
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.get("/",(req, res) => {
